f3349a57c26549a64fa4b3427223a50354211d31
Iris-Ares <1241153976@qq.com>
2023-11-20 16:50:38 +0800

    ✨ fancy background test

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -3,32 +3,42 @@ import Link from 'next/link'
 import { useTranslation } from '@/app/i18n'
 import { Welcome } from '@/lib/components'
 
+import AnimatedBackground from '@/lib/components/AnimatedBackground'
+
+
 export default async function Home({ params: { lng } }:any) {
   const { t } = await useTranslation(lng)
   return (
-    <div className='h-full w-full flex items-center justify-center flex-col relative'>
-
-        <Link href={`/${lng}/dashboard`}><span className='text-4xl font-extrabold'>{t('happy')}</span></Link>
-        
-        <h2 className='animate-module_show_fade text-4xl lg:text-6xl mb-6 mt-6 text-center' >{t('message')}</h2>
-
-        
-
-        <Welcome 
-        sentences={
-          [
-            t('welcome-1'),
-            t('welcome-2'),
-            t('welcome-3'),
-            t('welcome-4'),
-            t('welcome-5'),
-            t('welcome-6'),
-            t('welcome-7'),
-          ]
-        }
-        />
-
-        <button className='btn mt-4' title='developing' disabled><h3 className='animate-module_show_fade text-xl' >{t('developing')}</h3></button>
-    </div>
+    <>
+      
+      <div className='h-full w-full flex items-center justify-center flex-col relative '>
+  
+          <div className='z-10 flex items-center justify-center flex-col rounded-box p-4  backdrop-blur-sm'>
+            <Link href={`/${lng}/dashboard`}><span className='text-4xl font-extrabold'>{t('happy')}</span></Link>
+            
+            <h2 className='animate-module_show_fade text-4xl lg:text-6xl mb-6 mt-6 text-center' >{t('message')}</h2>
+    
+            
+    
+            <Welcome 
+            sentences={
+              [
+                t('welcome-1'),
+                t('welcome-2'),
+                t('welcome-3'),
+                t('welcome-4'),
+                t('welcome-5'),
+                t('welcome-6'),
+                t('welcome-7'),
+              ]
+            }
+            />
+    
+            <button className='btn mt-4' title='developing' disabled><h3 className='animate-module_show_fade text-xl' >{t('developing')}</h3></button>
+          </div>
+          
+      </div>
+      <AnimatedBackground />
+    </>
   )
 }
---
+++ lib/components/AnimatedBackground/index.tsx
@@ -0,0 +1,16 @@
+'use client'
+import Spline from '@splinetool/react-spline';
+import './index.css'
+
+const AnimatedBackground:React.FC = () =>{
+
+
+
+    return (
+        <div className='AnimatedBackground absolute left-0 top-0 z-0 w-full h-full opacity-60'>
+            <Spline scene="https://prod.spline.design/ImkOUMhPq3na0fUI/scene.splinecode" />
+        </div>
+    )
+}
+
+export default AnimatedBackground
