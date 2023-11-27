851ec4873b39cd6a633d0112d1cb8b698673fdd3
Iris-Ares <1241153976@qq.com>
2023-11-27 17:16:20 +0800

    ⚡️ background lazy load

--- lib/apps/AnimatedBackground.tsx
+++ lib/apps/AnimatedBackground.tsx
@@ -1,21 +1,23 @@
 'use client'
-import Spline from '@splinetool/react-spline';
-import { useState ,Suspense } from 'react';
+// import Spline from '@splinetool/react-spline';
+import { useState ,Suspense , lazy} from 'react';
 
 import type { Application } from '@splinetool/runtime';
 
+const Spline = lazy(() => import('@splinetool/react-spline'));
 
 const AnimatedBackground:React.FC = () =>{
     const [ App , setApp ] = useState<Application | null>(null)
 
 
     return (
-        <div className='absolute left-0 top-0 z-10 w-full h-full  opacity-80'>
-            <Suspense fallback={<div className='w-full h-full flex items-center justify-center'><span className="loading loading-ring loading-lg scale-[6]"></span></div>}>
+        <div className='absolute left-0 top-0 z-10 w-full h-full  opacity-80 overflow-hidden'>
+            <Suspense >
                 <Spline className={` ${App ? '' : 'hidden'} `} scene="https://prod.spline.design/BENrjy1JDnIE1AAP/scene.splinecode"  
                 onLoad={setApp}
                 />
             </Suspense>
+            <div className={`${App ? 'hidden' : ''} w-full h-full flex items-center justify-center`}><span className="loading loading-ring loading-lg scale-[6]"></span></div>
         </div>
     )
 }
